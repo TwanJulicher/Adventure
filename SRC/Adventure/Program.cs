97f30f4ec73a3cd44cd77a375f7ab5b3b2113cfd
twannetjuh <twan_1993@hotmail.com>
2014-05-06 16:23:07 +0200

    no message

---
+++ SRC/Adventure/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AdventureJourney
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welkom bij Adventure Journey");
+            Game_Start();
+        }
+
+        static void Game_Start()
+        {
+
+            int Menu_Choice;
+            Console.WriteLine("Wat wil je doen?");
+            Console.WriteLine("1: Beginnen");
+            Console.WriteLine("2: Informatie");
+            Console.WriteLine("3: Afsluiten");
+            Menu_Choice = int.Parse(Console.ReadLine());
+
+            switch (Menu_Choice)
+            {
+                case 1:
+                    //Adventure_start();
+                    break;
+                case 2:
+                    Console.WriteLine("In Ontwikkeling");
+                    Console.ReadLine();
+                    Game_Start();
+                    break;
+                case 3:
+                    Console.WriteLine("Totziens!");
+                    Console.ReadLine();
+                    return;
+                default:
+                    Console.WriteLine("Ongeldig antwoord.");
+                    Console.ReadLine();
+                    Game_Start();
+                    break;
+
+            }
+        }
+    }
+}
